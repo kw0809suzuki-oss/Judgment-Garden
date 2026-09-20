2de0b1d6e9a954c706382d0740840356aaf00ff5
ストラクYS <kw.0809suzuki@gmail.com>
2026-09-20 15:46:44 +0900

    Organize mainline adapter: judgment_garden/mainline/__init__.py

---
+++ judgment_garden/mainline/__init__.py
@@ -0,0 +1,5 @@
+"""Adapters that connect Judgment Garden to Kaggriculture mainline data/runtime.
+
+These modules are inputs/bridges from the Kaggriculture mainline.
+They are not Garden learning logic and do not imply mainline adoption.
+"""
